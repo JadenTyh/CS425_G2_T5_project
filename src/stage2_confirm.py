734ab53b9e5f75f67b7cfcc38569b0c4a3a22465
Coben Yap <cobenyapsh@gmail.com>
2025-11-10 16:02:02 +0800

    expanded yes no possible responses

--- src/stage2_confirm.py
+++ src/stage2_confirm.py
@@ -1,7 +1,22 @@
-def is_yes(text):
+def is_yes(text: str) -> bool:
     text = text.lower().strip()
-    return text in ["yes","yeah","yup","sure","go ahead","do it","ok","please","play","start","proceed"]
 
-def is_no(text):
+    YES_PATTERNS = [
+        "yes", "yeah", "yea", "yep", "yup", "sure", "ok", "okay", "k",
+        "do it", "go ahead", "please", "play", "start", "fine", "ig", "i guess",
+        "why not", "sounds good", "bet", "ight", "do that", "alright", "go for it"
+    ]
+
+    return any(phrase in text for phrase in YES_PATTERNS)
+
+
+def is_no(text: str) -> bool:
     text = text.lower().strip()
-    return text in ["no","nah","nope","stop","cancel","not really"]
+
+    NO_PATTERNS = [
+        "no", "nah", "nope", "stop", "cancel", "don't", "dont", "not really",
+        "please no", "i don't want", "leave it", "nvm", "nevermind", "idc", "i don't care",
+        "no thanks", "i'm good", "im good", "not now", "stop it", "let's not"
+    ]
+
+    return any(phrase in text for phrase in NO_PATTERNS)
